0e89fa8e322d491545ace0414caebfeb06e2ead3
ari7946 <mercado7946@gmail.com>
2020-01-10 15:53:14 -0800

    Add discount for round trips

--- src/components/FormHeader.js
+++ src/components/FormHeader.js
@@ -4,6 +4,9 @@ import { Badge, Row, Col, ListGroupItem, ListGroup } from 'reactstrap';
 function FormHeader(props) {
   const { startAddress, endAddress, points, price, dropFee, direction } = props.state;
   const total = Number(price) + dropFee;
+  const discount = total * 0.20;
+  const discountTotal = total - discount;
+
   return (
     <div>
       {startAddress && (
@@ -15,19 +18,21 @@ function FormHeader(props) {
       )}
 
       {(points[0] && points[1]) && (
-          <div className="text-center mx-auto my-4 w-75">
-            {/* <p><Badge color="info">Distance: </Badge><br />{distance} mi</p> */}
-            <p className='ml-4 mb-0 text-monospace'>Taxi Fare: ${price}</p>
-            <p className="text-monospace mb-0"><span className="mr-3 lead">+</span>Drop Fee: ${dropFee}.00</p>
-            {direction === 'oneWay' 
-              ? <p className='ml-3 text-monospace price pt-2'>Total: ${total}</p>
-              : (
-                <>
-                  <p className='ml-3 text-monospace price pt-2'>Total: ${total}</p>
-                </>
-              )
-            }
-          </div>
+        <div className="text-center mx-auto my-4 w-75">
+          {/* <p><Badge color="info">Distance: </Badge><br />{distance} mi</p> */}
+          <p className='ml-4 mb-0 text-monospace'>Taxi Fare: ${price}</p>
+          <p className="text-monospace mb-0"><span className="mr-3 lead">+</span>Drop Fee: ${dropFee}.00</p>
+          {direction === 'oneWay' 
+            ? <p className='ml-3 text-monospace price pt-2'>Total: ${total}</p>
+            : (
+              <>
+                <p className='ml-3 text-monospace price pt-2 mb-0'>Subtotal: ${total}</p>
+                <p className='ml-3 text-monospace pt-0 mb-0'><span className="mr-3 lead">-</span>Discount: ${discount}</p>
+                <p className='ml-3 text-monospace price pt-2'>Total: ${discountTotal}</p>
+              </>
+            )
+          }
+        </div>
       )}
     </div>
   )

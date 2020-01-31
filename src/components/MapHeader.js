f9dec7a7dc6b409e20839992f4ba9395ca3ffec5
ari7946 <mercado7946@gmail.com>
2020-01-30 16:01:24 -0800

    Add map header

--- src/components/MapHeader.js
+++ src/components/MapHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Badge } from 'reactstrap';
+import { Badge, Button } from 'reactstrap';
 
 function MapHeader(props) {
   const { startAddress, endAddress, points, price, dropFee, direction } = props.state;
@@ -19,10 +19,13 @@ function MapHeader(props) {
                 <h4 className="mb-0">Please Select <Badge color="danger">Destination</Badge></h4>
               ) : (
                   <>
-                    <h4 className="mb-0"><Badge color="info">Estimate:</Badge> 
-                      ${direction === 'oneWay' ? total : discountTotal}
-                    </h4>
-                    <p>714-432-1222</p>
+                    <div>
+                      <h4 className="mb-0 estimate"><Badge color="info">Estimate:</Badge> 
+                        ${direction === 'oneWay' ? total : discountTotal}
+                      </h4>
+                      <h2>Call to reserve taxi: 714-432-1222</h2>
+                      <Button color='warning'>Reserve Taxi Online</Button>
+                    </div>
                   </>
                 )
             )

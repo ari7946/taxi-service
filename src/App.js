90ca4b52e5e23e7d9a8d82aad00696c7b48af465
ari7946 <mercado7946@gmail.com>
2020-02-03 23:22:44 -0800

    Change bg to white

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App bg-light">
+      <div className="App">
         <NavbarContainer />
         <Switch>
           <Route path='/' exact component={Landing} /> 

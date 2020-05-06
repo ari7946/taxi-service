0f09ee14a0886c303508612d9e2529364a4104ba
ari7946 <mercado7946@gmail.com>
2020-05-05 22:27:31 -0700

    Change naming to be more consistent

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import MapFormContainer from './components/book/MapFormContainer';
 import NavbarContainer from './components/navbar/NavbarContainer';
 import Landing from './components/landing/Landing';
 import About from './components/about/About';
-import Container from './components/admin/Container';
+import TripsContainer from './components/admin/TripsContainer';
+import Login from './components/admin/Login';
 import { Route, Switch } from 'react-router-dom';
 import './index.css';
 import './App.css';
@@ -18,9 +19,8 @@ class App extends Component {
           <Route path='/' exact component={Landing} /> 
           <Route path='/about' exact component={About} /> 
           <Route path='/book' exact component={MapFormContainer} />
-          <Route path='/admin' exact component={Container} />
-          {/* <Route path='/admin/edit/:id' exact component={EditTrip} /> 
-          <Route path='/admin/delete/:id' exact component={DeleteTrip} /> */}
+          <Route path='/admin' exact component={Login} />
+          <Route path='/admin/trips' exact component={TripsContainer} /> 
         </Switch>
       </div>
     );

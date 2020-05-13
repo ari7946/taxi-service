b6ccad0f41d84b02499cd0fcea96a3c89844abbc
ari7946 <mercado7946@gmail.com>
2020-05-12 18:10:06 -0700

    Add utility classes and style navbar

--- src/components/navbar/NavbarContainer.js
+++ src/components/navbar/NavbarContainer.js
@@ -18,30 +18,24 @@ const NavbarComponent = (props) => {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <Navbar className="container mb-5" expand="md">
-      <NavbarBrand href="/" className="mr-auto">
+    <Navbar className="container mb-5 mt-3" expand="md">
+      <NavbarBrand href="/" className="mr-auto text-grey-light-2">
         <FontAwesomeIcon className="mr-2 fa-lg" icon={faTaxi} />
         Coastal Yellow Cabs
       </NavbarBrand>
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
         <Nav className="mx-auto" navbar>
-          <NavItem>
-            <NavLink className="px-4 py-3" href="/">
-              <Link className="" to="/" style={{ textDecoration: 'none' }}>Home</Link>
-            </NavLink>
+          <NavItem className='py-3'>
+            <Link className="px-5 mr-3 py-3 text-yellow lead" to="/" style={{ textDecoration: 'none' }}>Home</Link>
           </NavItem>
 
-          <NavItem>
-            <NavLink className="px-4 py-3" href="/">
-              <Link className="" to="/book" style={{ textDecoration: 'none' }}>Book</Link>
-            </NavLink>
+          <NavItem className='py-3'>
+            <Link className="px-5 mx-2 py-3 text-yellow lead" to="/book" style={{ textDecoration: 'none' }}>Book</Link>
           </NavItem>
 
-          <NavItem>
-            <NavLink className="px-4 py-3" href="/">
-              <Link className="" to="/about" style={{ textDecoration: 'none' }}>About</Link>
-            </NavLink>
+          <NavItem className='py-3'>
+            <Link className="px-5 ml-3 py-3 text-yellow lead" to="/about" style={{ textDecoration: 'none' }}>About</Link>
           </NavItem>
 
           {/* <NavItem>

e9f9e527a18d768e10c568d54de1b209644fe31d
ari7946 <mercado7946@gmail.com>
2020-05-14 16:08:54 -0700

    Switch button location

--- src/components/admin/Trip.js
+++ src/components/admin/Trip.js
@@ -16,33 +16,8 @@ const Trip = (props) => {
       <p><span className="trip-list-heading">email:</span> {trip.email}</p>
       <p><span className="trip-list-heading">start address:</span> {trip.startAddress}</p>
       <p><span className="trip-list-heading">destination:</span> {trip.endAddress}</p>
-        <Button
-          className="mr-1"
-          color="secondary"
-          id={"Popover-" + trip.id}
-          type="button"
-        >
-          Trip Details
-        </Button>
-        <Popover
-          placement='bottom'
-          isOpen={popoverOpen}
-          target={"Popover-" + trip.id}
-          toggle={toggle}
-        >
-          <PopoverBody>
-            <ListGroup>
-              <ListGroupItem><span className="font-weight-bold">Estimate: </span>${total}</ListGroupItem>
-              <ListGroupItem><span className="font-weight-bold">Distance: </span> {trip.distance} miles</ListGroupItem>
-              <ListGroupItem><span className="font-weight-bold">Rate: </span> {trip.vehicle === 'sedan' ? "$2.95 per mile" : '$3.95 per mile'}</ListGroupItem>
-              <ListGroupItem><span className="font-weight-bold">Vehicle: </span>{trip.vehicle}</ListGroupItem>
-              <ListGroupItem><span className="font-weight-bold">passengers: </span>{trip.vehicle === 'sedan' ? '1 - 4' : '1 - 7'}</ListGroupItem>
-            </ListGroup>
-          </PopoverBody>
-        </Popover>
 
-
-      <ButtonGroup className='ml-3' color="light">
+      <ButtonGroup color="light">
         <Button
           className={`
             ${trip.status === 'confirm' ? 'bg-green-light text-green-dark' : null}
@@ -61,6 +36,31 @@ const Trip = (props) => {
         </Button>
         <Button onClick={() => props.removeTrip(trip.id)}>Delete</Button>
       </ButtonGroup>
+
+      <Button
+        className="ml-3"
+        color="secondary"
+        id={"Popover-" + trip.id}
+        type="button"
+      >
+        Trip Details
+      </Button>
+      <Popover
+        placement='bottom'
+        isOpen={popoverOpen}
+        target={"Popover-" + trip.id}
+        toggle={toggle}
+      >
+        <PopoverBody>
+          <ListGroup>
+            <ListGroupItem><span className="font-weight-bold">Estimate: </span>${total}</ListGroupItem>
+            <ListGroupItem><span className="font-weight-bold">Distance: </span> {trip.distance} miles</ListGroupItem>
+            <ListGroupItem><span className="font-weight-bold">Rate: </span> {trip.vehicle === 'sedan' ? "$2.95 per mile" : '$3.95 per mile'}</ListGroupItem>
+            <ListGroupItem><span className="font-weight-bold">Vehicle: </span>{trip.vehicle}</ListGroupItem>
+            <ListGroupItem><span className="font-weight-bold">passengers: </span>{trip.vehicle === 'sedan' ? '1 - 4' : '1 - 7'}</ListGroupItem>
+          </ListGroup>
+        </PopoverBody>
+      </Popover>
     </ListGroupItem>
   )
 }

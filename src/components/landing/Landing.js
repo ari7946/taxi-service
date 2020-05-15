f7135e0ed7eb768f1dcb1af7c6bc7f828b7753a0
ari7946 <mercado7946@gmail.com>
2020-05-14 23:13:26 -0700

    Remove commented out code on Landing page

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -5,17 +5,6 @@ import { Link, withRouter } from 'react-router-dom'
 const Landing = (props) => {
   return (
     <Container fluid>
-      {/* <Jumbotron className='landing'>
-        <div className='landing-header'>
-          <h1 className="display-4 text-warning">Welcome to Cerritos Yellow Cab!</h1>
-          <hr className="my-2" />
-          <p className="lead">Our professional, rigorously trained drivers will accomodate your needs.</p>
-          <p>Use our online system to book a taxi and get rate right away</p>
-        </div>
-        <Button color="warning">
-          <Link to="/book" style={{ textDecoration: 'none', color: 'black' }}>Book a Taxi</Link>
-        </Button>
-      </Jumbotron> */}
       <Jumbotron className='landing'>
         <div className='landing-header'>
           <h1 className="display-4 text-white text-uppercase">Coastal <span className='landing-yellow'>Yellow</span> Cabs</h1>

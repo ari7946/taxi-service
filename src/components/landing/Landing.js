292b8ea32051e0210a2378310366b349a11d5891
ari7946 <mercado7946@gmail.com>
2020-05-29 20:11:27 -0700

    Add saturation to bg

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -14,9 +14,9 @@ const Landing = (props) => {
           <hr className="my-2" />
           <h2 className="text-white lead">Our professional, rigorously trained drivers will accomodate your needs.</h2>
           <p className="text-white">Use our online system to book a taxi and get rate right away</p>
+          <Link className="btn btn-lg landing-button" to="/book" style={{ textDecoration: 'none', color: 'black' }}>Book a Taxi</Link>
+          <Link className="btn btn-lg ml-3 landing-button" to="/book" style={{ textDecoration: 'none', color: 'black' }}>Get An Estimate</Link>
         </div>
-        <Link className="btn btn-lg landing-button" to="/book" style={{ textDecoration: 'none', color: 'black' }}>Book a Taxi</Link>
-        <Link className="btn btn-lg ml-3 landing-button" to="/book" style={{ textDecoration: 'none', color: 'black' }}>Get An Estimate</Link>
       </Jumbotron>
     </Container>
   );

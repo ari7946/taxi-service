1200df7fc769d2a22aece63605e04f0ac7e5a2f0
ari7946 <mercado7946@gmail.com>
2020-06-02 15:56:28 -0700

    Refactor MapHeader to useBookApi

--- src/components/book/MapHeader.js
+++ src/components/book/MapHeader.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Badge } from 'reactstrap';
+import { useBookApi } from './BookApi';
 
 function MapHeader(props) {
-  const { startAddress, endAddress } = props.state;
+  const { state } = useBookApi();
   const startHeading = <span className="address-heading-starting">Starting Point</span>;
   const destinationHeading = <span className="address-heading-destination">Destination</span>;
 
   return (
     <div className='mb-3'>
-      {!startAddress && !endAddress && (
+      {!state.startAddress && !state.endAddress && (
         <h4 className="mb-0 text-white map-heading">Please Select {startHeading} and {destinationHeading}</h4>
       )}
 
-      {!startAddress && endAddress && (
+      {!state.startAddress && state.endAddress && (
         <h4 className="mb-0 text-white map-heading">Please Select {startHeading}</h4>
       )} 
 
-      {startAddress && !endAddress && (
+      {state.startAddress && !state.endAddress && (
         <h4 className="mb-0 text-white map-heading">Please Select {destinationHeading}</h4>
       )}
 
-      {startAddress && endAddress && (
+      {state.startAddress && state.endAddress && (
         <h4 className="mb-9 text-white map-heading">Please submit the form to book a taxi</h4>
       )} 
     </div>

d792757acbc48dea4b8b35532bff6d6913a64471
ari7946 <mercado7946@gmail.com>
2020-06-02 19:07:53 -0700

    Implement form submit and process form on BookApi

--- src/components/book/BookApi.js
+++ src/components/book/BookApi.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useContext, createContext, useReducer } from 'react';
 import axios from 'axios';
-import { useAuth } from '../../auth/use-auth';
 import { BookReducer, initialState } from './BookReducer';
 const bookContext = createContext();
 
@@ -15,9 +14,34 @@ export function useBookApi() {
 
 const useProvideBookApi = () => {
   const [state, dispatch] = useReducer(BookReducer, initialState);
-  //const { authHeaders } = useAuth();
+  const { distance, startAddress, endAddress, price, name, comments, phone, passengers, email, direction, date, time, vehicle, status, submitted, valid } = state;
 
-  const setInput = (type, name, value) => {
+  useEffect(() => {
+    if (submitted && valid) {
+      _processForm()
+    } else if (state.submitted && !state.valid) {
+      dispatch({ type: 'error', errorMessage: 'One or more fields are invalid' })
+    }
+  }, [submitted, valid])
+
+  const _processForm = async () => {
+    const body = { distance, startAddress, endAddress, price, name, comments, phone, passengers, email, direction, date, time, vehicle, status }
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_TRIPS}/api/trips`, body)
+      if (res) {
+        dispatch({ type: 'success' })
+      }
+    } catch (error) {
+      dispatch({ type: 'error', errorMessage: error });
+    }
+  }
+
+  const submitForm = (formSubmitEvent) => {
+    formSubmitEvent.preventDefault();
+    dispatch({ type: 'submit' })
+  }
+
+  const setInput = ({ type, name, value }) => {
     dispatch({ type, name, value })
   }
 
@@ -34,6 +58,7 @@ const useProvideBookApi = () => {
   }
 
   return {
+    submitForm,
     locationsFound,
     locationsCleared,
     routeChanged,

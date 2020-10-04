80dc07d32b32eb39d6ddf6a1ba8199300ff9ee94
ari7946 <mercado7946@gmail.com>
2020-10-03 17:42:13 -0700

    Include auth in dependency useEffect array

--- src/components/trips/trip-list.component.js
+++ src/components/trips/trip-list.component.js
@@ -1,22 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Spinner } from 'reactstrap';
-import AdminNav from './trip-nav.component';
+import AdminNav from './trip-admin-nav.component';
 import AdminTabContent from './trip-tab-content.component';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import './trips.styles.css';
+import { useAuth } from '../../auth/use-auth';
 
 import { getTrips } from '../../redux/trips/trips.actions';
 import { selectAllTrips, selectLoadingType } from '../../redux/trips/trips.selectors';
 
 const TripList = ({ getTrips, loadingType }) => {
   const [activeTab, setActiveTab] = useState('viewAll');
+  const { auth } = useAuth();
 
   useEffect(() => {
     getTrips();
-  }, [])
-  console.log('hello')
+  }, [auth])
+
   return (
     <Container fluid>
 

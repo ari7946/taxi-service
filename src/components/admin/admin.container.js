be556ca2875ed6b333d25edd2886d87dffa940b3
ari7946 <mercado7946@gmail.com>
2020-10-03 17:36:14 -0700

    Remove commented out code

--- src/components/admin/admin.container.js
+++ src/components/admin/admin.container.js
@@ -8,29 +8,6 @@ import TripList from '../trips';
 
 const AdminContainer = () => {
   const { auth } = useAuth();
-  // const [trips, setTrips] = useState([]);
-  // const [loading, setLoading] = useState(false)
-  // const [error, setError] = useState('');
-
-  // const getTrips = async () => {
-  //   setLoading(true);
-  //   try {
-  //     const result = await axios.get(`${process.env.REACT_APP_TRIPS}/api/trips`, authHeaders);
-  //     setTrips(result.data);
-  //     setLoading(false)
-  //   } catch (error) {
-  //     setLoading(false);
-  //     setError('Unable to get requested trips')
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   getTrips();
-  // }, [])
-
-  // if (loading) {
-  //   return <Spinner />
-  // }
 
   return (
     <Container>

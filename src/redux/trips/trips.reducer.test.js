3506cb05f62c9bd0f245ce915fa87859a574bd47
ari7946 <mercado7946@gmail.com>
2020-12-22 17:46:47 -0800

    Add unit test to trips reducer

--- src/redux/trips/trips.reducer.test.js
+++ src/redux/trips/trips.reducer.test.js
@@ -1,12 +1,6 @@
 import TripsActionTypes from './trips.types';
 import tripsReducer from './trips.reducer';
-// const TripsActionTypes = {
-//   SUBMIT: 'SUBMIT',
-//   GET_TRIPS: 'GET_TRIPS',
-//   UPDATE_TRIP: 'UPDATE_TRIP',
-//   DELETE_TRIP: 'DELETE_TRIP',
-//   ERROR: 'ERROR',
-// }
+
 const initialState = {
   trips: [],
   loadingType: '',
@@ -23,5 +17,71 @@ describe('tripsReducer', () => {
     expect(tripsReducer(initialState, {type: TripsActionTypes.SUBMIT, tripId: 2, loadingType: 'confirm'})
       .loadingTripId
     ).toBe(2)
+  })
+
+  it('should get all trips in ascending order', () => {
+    const mockTrips = [{
+      id: 1,
+      status: 'pending'
+    }, {
+      id: 2,
+      status: 'confirm'
+    }]
+    expect(tripsReducer(initialState, {type: TripsActionTypes.GET_TRIPS, trips: mockTrips})
+      .trips
+    ).toEqual(mockTrips.reverse())
+  })
+
+  it('should delete trip with id of 1', () => {
+    const mockTrips = [{
+      id: 1,
+      status: 'pending'
+    }, {
+      id: 2,
+      status: 'confirm'
+    }]
+    const mockState = {
+      trips: mockTrips,
+      loadingType: '',
+      error: '',
+      loadingTripId: null,
+    }
+    const deletedTrip = {
+      id: 1,
+      status: 'pending'
+    }
+    const remainingTrips = [{
+      id: 2,
+      status: 'confirm'
+    }]
+
+    expect(tripsReducer(mockState, { type: TripsActionTypes.DELETE_TRIP, trip: deletedTrip })
+      .trips
+    ).toEqual(remainingTrips)
+  })
+
+  it('should update trip of id 2 status to archive', () => {
+    const mockTrips = [{
+      id: 1,
+      status: 'pending'
+    }, {
+      id: 2,
+      status: 'confirm'
+    }]
+    const mockState = {
+      trips: mockTrips,
+      loadingType: '',
+      error: '',
+      loadingTripId: null,
+    }
+    const mockTripWithUpdatedStatus = {
+      id: 2,
+      status: 'archive'
+    }
+
+    expect(tripsReducer(mockState, { type: TripsActionTypes.UPDATE_TRIP, trip: mockTripWithUpdatedStatus})
+      .trips[1].status
+    ).toEqual('archive')
+
   })
 })

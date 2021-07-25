a9e5f0dbc21d7fd18635ba2cc228a2bc9592d0d5
ari7946 <mercado7946@gmail.com>
2021-07-25 11:50:14 -0700

    Minor updates agn

--- src/components/book/book-estimate/book-estimate.styles.js
+++ src/components/book/book-estimate/book-estimate.styles.js
@@ -1,15 +1,13 @@
 import styled from 'styled-components'
 
 export const EstimateContainer = styled.div` 
-  & {
-    background-color: var(--color-green-dark) !important;
-    color: var(--color-green-light);
-    border: none !important;
-    font-family: var(--font-monospaced-bold);
-    letter-spacing: 0.25rem;
-    margin: 2rem 0;
-    text-transform: uppercase;
-  }
+  background-color: var(--color-green-dark) !important;
+  color: var(--color-green-light);
+  border: none !important;
+  font-family: var(--font-monospaced-bold);
+  letter-spacing: 0.25rem;
+  margin: 2rem 0;
+  text-transform: uppercase;
 
   .estimate-info {
     margin: 0 auto;
@@ -36,22 +34,15 @@ export const EstimateContainer = styled.div`
   }
 
   @media (max-width: 1200px) {
-    &, .price {
-      font-size: 20px;
-    }
+    font-size: 20px;
   }
 
   @media (max-width: 600px) {
-    & {
-      font-family: var(--font-monospaced);
-      letter-spacing: 0.2rem;
-    }
+    font-family: var(--font-monospaced);
+    letter-spacing: 0.2rem;
   }
 
   @media (max-width: 415px) {
-    & {
-      font-family: var(--font-monospaced);
-      letter-spacing: 0.1rem;
-    }
+    letter-spacing: 0.1rem;
   }
 `

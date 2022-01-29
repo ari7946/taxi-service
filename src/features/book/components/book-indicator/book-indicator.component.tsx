a2ce38fd5e01d208351a5e7cac755d808acbae66
ari7946 <mercado7946@gmail.com>
2022-01-29 15:55:07 -0800

    Add props interface and include optional sectionName field for conditional styling

--- src/features/book/components/book-indicator/book-indicator.component.tsx
+++ src/features/book/components/book-indicator/book-indicator.component.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import * as Styled from './book-indicator.styles';
 
-import { selectStartAddressAndEndAddressAreValid } from '../../redux/book.selectors';
+interface IndicatorProps {
+  num: number;
+  isActive: boolean;
+  sectionName?: string;
+}
 
-const Indicator = function ({ num, isActive }: { num: number; isActive: boolean }) {
-  return <Styled.IndicatorWrapper isActive={isActive}>{num}</Styled.IndicatorWrapper>;
+const Indicator = function ({ num, isActive, sectionName }: IndicatorProps) {
+  return (
+    <Styled.IndicatorWrapper isActive={isActive} sectionName={sectionName}>
+      {num}
+    </Styled.IndicatorWrapper>
+  );
 };
 
 export default Indicator;

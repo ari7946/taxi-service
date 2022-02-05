490c62aad258a4da409c4427af6acd3dba86b231
ari7946 <mercado7946@gmail.com>
2022-02-04 17:22:41 -0800

    Move business logic to book selector and away from estimate component

--- src/features/book/components/book-estimate/book-estimate.component.tsx
+++ src/features/book/components/book-estimate/book-estimate.component.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 // import './book-estimate.styles.css';
+import ReactTextTransition, { presets } from 'react-text-transition';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import {
-  selectVehicle,
   selectPrice,
   selectDropFee,
   selectDirection,
@@ -14,17 +14,37 @@ import {
 
 import { EstimateContainer } from './book-estimate.styles';
 
-const Estimate = ({ taxiFare, estimate, dropFee, vehicle }) => {
+const Estimate = ({ taxiFare, estimate, dropFee }) => {
   return (
     <EstimateContainer>
       <div className="estimate-info">
-        <p className="fare">Taxi Fare: ${vehicle ? taxiFare : '0.00'}</p>
+        <p className="fare"> Taxi Fare: ${taxiFare}</p>
         <p className="drop-fee">
           <span className="plus-sign">+</span>
           Drop Fee: ${dropFee}.00
         </p>
 
-        <h2 className="price">Estimate: ${vehicle ? estimate : '0.00'}</h2>
+        <h2 className="price">
+          Estimate: $
+          {estimate
+            .toString()
+            .split('')
+            .map((letter, i) => (
+              <ReactTextTransition
+                key={i}
+                text={letter}
+                style={
+                  letter === ' ' && {
+                    margin: '0 5px'
+                  }
+                }
+                delay={i * 100}
+                springConfig={presets.molasses}
+                inline
+                noOverflow
+              />
+            ))}
+        </h2>
       </div>
     </EstimateContainer>
   );
@@ -35,8 +55,7 @@ const mapStateToProps = createStructuredSelector({
   dropFee: selectDropFee,
   direction: selectDirection,
   taxiFare: selectTaxiFare,
-  estimate: selectValidEstimate,
-  vehicle: selectVehicle
+  estimate: selectValidEstimate
 });
 
 export default connect(mapStateToProps)(Estimate);

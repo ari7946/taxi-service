4f0db83e626e0ea87a79e46df362899e5d4e7df3
ari7946 <mercado7946@gmail.com>
2022-02-26 23:10:02 -0800

    Wrap navlink to Logo component

--- src/features/navbar/components/navbar-logo/navbar-logo.component.jsx
+++ src/features/navbar/components/navbar-logo/navbar-logo.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 import logo2 from '../../assets/cyc-brand.png';
 import * as Styled from './navbar-logo.styles.js';
@@ -6,7 +7,9 @@ import * as Styled from './navbar-logo.styles.js';
 export default function NavbarLogo() {
   return (
     <Styled.LogoWrapper>
-      <img src={logo2} alt="Coastal Yellow Cabs Logo" className="logo" />
+      <NavLink to='/'>
+        <img src={logo2} alt="Coastal Yellow Cabs Logo" className="logo" />
+      </NavLink>
     </Styled.LogoWrapper>
   )
 }

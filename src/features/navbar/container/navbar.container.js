60e456795c90910c9fd4797b6dfbab5ac16f95bb
ari7946 <mercado7946@gmail.com>
2022-02-26 23:08:36 -0800

    Provide navbar container context

--- src/features/navbar/container/navbar.container.js
+++ src/features/navbar/container/navbar.container.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import { MobileMenuOpenProvider } from '../context/MobileMenuOpenProvider';
+
 import NavbarDefault from '../components/navbar-wrapper/navbar-wrapper-default.component';
 import NavbarAdmin from '../components/navbar-wrapper/navbar-wrapper-admin.component';
 import NavbarUser from '../components/navbar-wrapper/navbar-wrapper-user.component';
@@ -10,11 +12,11 @@ import { selectAuthRole } from '../../auth/redux/auth.selectors';
 
 const NavbarContainer = ({ authRole }) => {
   return (
-    <>
+    <MobileMenuOpenProvider>
       {authRole !== 'admin' && authRole !== 'user' && <NavbarDefault />}
       {authRole === 'admin' && <NavbarAdmin />}
       {authRole === 'user' && <NavbarUser />}
-    </>
+    </MobileMenuOpenProvider>
   );
 };
 

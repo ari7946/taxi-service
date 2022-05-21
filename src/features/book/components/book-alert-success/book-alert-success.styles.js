b31a3ddc12a760c9d84bf348788b337d71a30bd7
ari7946 <mercado7946@gmail.com>
2022-05-20 18:05:10 -0700

    Update alert success to only display when a user successfully books a taxi, and goes away when a user deselects starting point or location

---
+++ src/features/book/components/book-alert-success/book-alert-success.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const BookAlertSuccessWrapper = styled.p`
+  font-size: 1.5rem;
+  background-color: var(--color-green-light);
+  font-color: black;
+  width: 100%;
+  text-align: center;
+  margin-top: -5rem;
+  padding: 1rem 0;
+`;

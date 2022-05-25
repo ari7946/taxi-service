55bb680eb9aa52e3c6bd7002a9e90794042cf99a
ari7946 <mercado7946@gmail.com>
2022-05-25 10:48:29 -0700

    Fix navbar bug

--- src/features/navbar/context/MobileMenuOpenProvider.tsx
+++ src/features/navbar/context/MobileMenuOpenProvider.tsx
@@ -1,12 +1,11 @@
-import React, { useState, createContext, useMemo, useCallback } from 'react';
+import React, { useState, createContext } from 'react';
 
 export const MobileMenuOpenContext = createContext(null);
 
 export const MobileMenuOpenProvider = ({ children }) => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
 
-  const toggleMobileMenu = () =>
-    useCallback(() => setMobileMenuOpen(!isMobileMenuOpen), [isMobileMenuOpen]);
+  const toggleMobileMenu = () => setMobileMenuOpen(!isMobileMenuOpen);
 
   return (
     <MobileMenuOpenContext.Provider value={{ isMobileMenuOpen, toggleMobileMenu }}>
--- src/features/navbar/hooks/useIsDesktop.tsx
+++ src/features/navbar/hooks/useIsDesktop.tsx
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 export default function useIsDesktop(): boolean {
   const [isDesktop, setDesktop] = useState<boolean>(true);
 
-  const updateMedia = () => {
+  const updateMedia = useCallback(() => {
     setDesktop(window.innerWidth > 800);
-  };
+  }, [isDesktop]);
 
   useEffect(() => {
     window.addEventListener('resize', updateMedia);

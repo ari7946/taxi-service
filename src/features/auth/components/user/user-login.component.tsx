a1aec38587c4456844fd234dc9d888eb6435c585
ari7946 <mercado7946@gmail.com>
2022-05-25 10:36:31 -0700

    Refactor user registeration and login with custom input and buttons

--- src/features/auth/components/user/user-login.component.tsx
+++ src/features/auth/components/user/user-login.component.tsx
@@ -1,71 +1,84 @@
 import React, { useState } from 'react';
-import { Container, Button, Form, FormGroup, Label, Input, Spinner } from 'reactstrap';
-import './user.styles.css';
+import * as Styled from './user.styles';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { userAuth } from '../../redux/auth.actions'
+import { userAuth } from '../../redux/auth.actions';
 import { selectAuthLoading } from '../../redux/auth.selectors';
-import { UserAuth } from '../../types/auth.types'
+import { UserAuth } from '../../types/auth.types';
 
-import { useHistory } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
+
+import LabeledInput from '../../../_global/components/labeled-input/labeled-input.component';
+import Button from '../../../_global/components/button/button.component';
 
 const UserLogin = ({ userAuth, loading }) => {
-  const [currentUsername, setCurrentUsername] = useState('');
-  const [currentPassword, setCurrentPassword] = useState('');
-  let history = useHistory();
+  const [userCredentials, setUserCredentials] = useState({
+    username: '',
+    password: '',
+  });
+
+  const history = useHistory();
+
+  const handleChange = (name: string, value: string) => {
+    setUserCredentials({
+      ...userCredentials,
+      [name]: value,
+    });
+  };
 
   return (
-    <Container className="text-green-light auth" fluid>
-      <h1 className="mb-3">Login</h1>
-      {loading && <Spinner size="md" color="light"></Spinner>}
-      <Form 
-        onSubmit={ async (event) => {
+    <Styled.UserAuthWrapper>
+      <h1>Login</h1>
+      {loading && <p>loading...</p>}
+      <form
+        onSubmit={async (event) => {
           event.preventDefault();
+          const { username, password } = userCredentials;
           await userAuth({
-            authType: 'login', 
-            username: currentUsername, 
-            password: currentPassword
+            authType: 'login',
+            username: username,
+            password: password,
           });
           history.push('/trips');
-        }}
-      >
-        <FormGroup>
-          <Label for="admin">Username</Label>
-          <Input
-            type="text"
-            name="username"
-            onChange={e => setCurrentUsername(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="admin">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            onChange={e => setCurrentPassword(e.target.value)}
-          />
-        </FormGroup>
-        <Button className="text-green-light">
-          Login
-        </Button>
-        <span className="ml-4">
-          Need to register?
-          <Link className="ml-3 text-grey-light-2" to="/register">Register here</Link>
-        </span>
-      </Form>
-    </Container>
-  )
-}
+        }}>
+        <LabeledInput
+          id="user-username"
+          type="text"
+          name="username"
+          placeholder="username"
+          handleChange={handleChange}
+          value={userCredentials.username}
+        />
+        <LabeledInput
+          id="user-password"
+          type="password"
+          name="password"
+          placeholder="password"
+          handleChange={handleChange}
+          value={userCredentials.password}
+        />
+
+        <Styled.ButtonGroupWrapper>
+          <Button type="submit" name="submit" primary>
+            Login
+          </Button>
+          <span>
+            Need to register?
+            <Link to="/register">Register here</Link>
+          </span>
+        </Styled.ButtonGroupWrapper>
+      </form>
+    </Styled.UserAuthWrapper>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   loading: selectAuthLoading,
-})
+});
 
-const mapDispatchToProps = dispatch => ({
-  userAuth: (userLoginData: UserAuth) => 
-    dispatch(userAuth({ ...userLoginData }))
-})
+const mapDispatchToProps = (dispatch) => ({
+  userAuth: (userLoginData: UserAuth) => dispatch(userAuth({ ...userLoginData })),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserLogin);

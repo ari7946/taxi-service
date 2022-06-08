5e0aad27926a100596cfdff2415b70d694869cb8
ari7946 <mercado7946@gmail.com>
2022-06-08 16:21:25 -0700

    import global components from single index file

---
+++ src/features/_global/components/index.ts
@@ -0,0 +1,5 @@
+import Spinner from './spinner/spinner.component';
+import Button from './button/button.component';
+import LabeledInput from './labeled-input/labeled-input.component';
+
+export { Spinner, Button, LabeledInput };

1f7e2d649c6e7e98c4a0224637d13a424e956445
ari7946 <mercado7946@gmail.com>
2022-06-08 16:24:12 -0700

    Refactor Trip Status to use new Spinner

--- src/features/trips/components/trip-status/trip-status.component.tsx
+++ src/features/trips/components/trip-status/trip-status.component.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import { Spinner } from 'reactstrap';
-
-import Button from '../../../_global/components/button/button.component';
 
 import { DefaultRootState, useDispatch, useSelector } from 'react-redux';
 
@@ -11,34 +8,17 @@ import { selectLoadingTrip } from '../../redux/trips.selectors';
 import { selectAuthRole } from '../../../auth/redux/auth.selectors';
 import { TripStatus as TripStatusType } from '../../types/trips.types';
 
+import { Spinner, Button } from '../../../_global/components';
+
 import * as Styled from './trip-status.styles';
 
-//! should probably move this to different file :/
-const SpinnerComponent = function () {
-  return (
-    <Spinner
-      as="span"
-      animation="border"
-      size="sm"
-      role="status"
-      aria-hidden="true"
-      className="mr-2"
-    />
-  );
-};
-
-//! Important note: This component has been refactored to redux hooks for trial
-
-// props passed not from redux
 interface TripStatusProps {
   tripId: number;
   tripStatus: TripStatusType;
 }
 
 const TripStatus = function ({ tripId, tripStatus }: TripStatusProps) {
-  // auth useSelector hook
   const authRole = useSelector(selectAuthRole);
-  // dispatch hook
   const dispatch = useDispatch();
 
   // handleLoadingTrip useSelector passes in the state obj using a HOF (returns a function).
@@ -68,7 +48,7 @@ const TripStatus = function ({ tripId, tripStatus }: TripStatusProps) {
               ${tripStatus === 'confirm' ? 'bg-green-light' : ''}
             `}
             handleClick={() => handleUpdateTrip('confirm', tripId)}>
-            {handleLoadingTrip(tripId, 'confirm') && <SpinnerComponent />}
+            {handleLoadingTrip(tripId, 'confirm') && <Spinner />}
             Confirm
           </Button>
 
@@ -77,7 +57,7 @@ const TripStatus = function ({ tripId, tripStatus }: TripStatusProps) {
               ${tripStatus === 'complete' ? 'bg-green-dark text-grey-light-2' : ''}
             `}
             handleClick={() => handleUpdateTrip('complete', tripId)}>
-            {handleLoadingTrip(tripId, 'complete') && <SpinnerComponent />}
+            {handleLoadingTrip(tripId, 'complete') && <Spinner />}
             Complete
           </Button>
 
@@ -86,12 +66,12 @@ const TripStatus = function ({ tripId, tripStatus }: TripStatusProps) {
               ${tripStatus === 'archive' ? 'bg-yellow' : ''}
             `}
             handleClick={() => handleUpdateTrip('archive', tripId)}>
-            {handleLoadingTrip(tripId, 'archive') && <SpinnerComponent />}
+            {handleLoadingTrip(tripId, 'archive') && <Spinner />}
             Archive
           </Button>
 
           <Button handleClick={() => handleDeleteTrip('delete', tripId)}>
-            {handleLoadingTrip(tripId, 'delete') && <SpinnerComponent />}
+            {handleLoadingTrip(tripId, 'delete') && <Spinner />}
             Delete
           </Button>
         </Styled.ButtonGroup>

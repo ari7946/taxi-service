cb078ca6dcaa2022fa3a2965761931598c6c1107
ari7946 <mercado7946@gmail.com>
2022-06-10 15:45:30 -0700

    Fixes Pane to use activeTab instead of trip status

--- src/features/trips/components/trip-tab-pane/trip-tab-pane.component.tsx
+++ src/features/trips/components/trip-tab-pane/trip-tab-pane.component.tsx
@@ -1,29 +1,28 @@
-import React, { useState } from 'react';
-import { TabPane, ListGroup } from 'reactstrap';
-import TripItem from '../trip-item/trip-item.component';
-import { Trip as TripObj, TripStatus, TabState } from '../../types/trips.types';
+import React from 'react';
 
-const TripTabPane = ({
-  tabId,
-  tripStatus,
-  trips,
-}: {
-  tabId: TabState;
-  tripStatus: TripStatus;
-  trips: TripObj[];
-}) => {
+import TripItem from '../trip-item/trip-item.component';
+import { Trip, TabState } from '../../types/trips.types';
+
+import * as Styled from './trip-tab-pane.styles';
+
+interface TripTabPaneProps {
+  activeTab: TabState;
+  trips: Trip[];
+}
+
+const TripTabPane = ({ activeTab, trips }: TripTabPaneProps) => {
   return (
-    <TabPane tabId={tabId}>
-      {trips.length ? (
+    <Styled.TapPaneWrapper>
+      {trips.length > 0 ? (
         <div>
           {trips.map((trip) => (
             <TripItem key={trip.id} trip={trip} />
           ))}
         </div>
       ) : (
-        <h4 className="text-green-light">There are no {tripStatus} trips.</h4>
+        <h4 className="text-green-light">There are no {activeTab} trips.</h4>
       )}
-    </TabPane>
+    </Styled.TapPaneWrapper>
   );
 };
 

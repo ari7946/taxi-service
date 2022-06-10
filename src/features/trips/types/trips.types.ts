c74af99a58fb28e9f069719d921cae0727b05023
ari7946 <mercado7946@gmail.com>
2022-06-10 15:46:44 -0700

    Remove bootstrap utility class and linter lints types file

--- src/features/trips/types/trips.types.ts
+++ src/features/trips/types/trips.types.ts
@@ -4,70 +4,74 @@ export const TripsActionTypes = {
   UPDATE_TRIP: 'UPDATE_TRIP',
   DELETE_TRIP: 'DELETE_TRIP',
   ERROR: 'ERROR',
-}
+};
 
 export interface ObjectLiteral {
   [key: string]: any;
 }
 
-export type TripStatus = 'pending' | 'confirm' | 'complete' | 'delete' | 'archive' | ''
+export type TripStatus = 'pending' | 'confirm' | 'complete' | 'delete' | 'archive' | '';
 
-// each trip loads when user or admin requests a status change. 
+// each trip loads when user or admin requests a status change.
 // 'getTrips' handles loading state for initial request to get ALL trips
-export type TripLoadingStatus = TripStatus | 'getTrips'
+export type TripLoadingStatus = TripStatus | 'getTrips';
 
 export interface Trip {
-  date: string
-  distance: number
-  email: string
-  endAddress: string
-  id: number
-  name: string
-  passengers: number
-  phone: number | string
-  price: number
-  startAddress: string
-  status: TripStatus
-  username: string
-  vehicle: string
+  date: string;
+  distance: number;
+  email: string;
+  endAddress: string;
+  id: number;
+  name: string;
+  passengers: number;
+  phone: number | string;
+  price: number;
+  startAddress: string;
+  status: TripStatus;
+  username: string;
+  vehicle: string;
 
   // optional
-  comments?: string | null
-  created_at?: string | number
-  direction?: string | null
-  time?: string | number
+  comments?: string | null;
+  created_at?: string | number;
+  direction?: string | null;
+  time?: string | number;
 }
 
-export type TabState = 'viewAll' | 'viewConfirmed' | 'viewCompleted' | 'viewArchived';
+export type TabState = 'all' | 'confirmed' | 'completed' | 'archived';
 
 // Trip Actions
 interface SubmitAction {
-  type: typeof TripsActionTypes.SUBMIT,
+  type: typeof TripsActionTypes.SUBMIT;
   payload: {
-    loadingType: string
-    loadingTripId?: number
-  }
+    loadingType: string;
+    loadingTripId?: number;
+  };
 }
 
 interface GetTripsAction {
-  type: typeof TripsActionTypes.GET_TRIPS,
-  payload: { trips: Trip[] }
+  type: typeof TripsActionTypes.GET_TRIPS;
+  payload: { trips: Trip[] };
 }
 
 interface UpdateTripsAction {
-  type: typeof TripsActionTypes.UPDATE_TRIP,
-  payload: { trip: Trip }
+  type: typeof TripsActionTypes.UPDATE_TRIP;
+  payload: { trip: Trip };
 }
 
 interface DeleteTripsAction {
-  type: typeof TripsActionTypes.DELETE_TRIP,
-  payload: { trip: Trip }
+  type: typeof TripsActionTypes.DELETE_TRIP;
+  payload: { trip: Trip };
 }
 
 interface ErrorAction {
-  type: typeof TripsActionTypes.ERROR,
-  payload: { error: string }
+  type: typeof TripsActionTypes.ERROR;
+  payload: { error: string };
 }
 
-export type Action = SubmitAction | GetTripsAction | UpdateTripsAction | DeleteTripsAction | ErrorAction
-
+export type Action =
+  | SubmitAction
+  | GetTripsAction
+  | UpdateTripsAction
+  | DeleteTripsAction
+  | ErrorAction;

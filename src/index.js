6eb47a246f56884012ed745e7060eb5c971be148
ari7946 <mercado7946@gmail.com>
2022-06-15 14:14:33 -0700

    Remove bootstrap, jquery, and reactstrap

--- src/index.js
+++ src/index.js
@@ -2,24 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { store, persistor } from './features/_global/redux/store';
 
 ReactDOM.render(
-  (
-    <Provider store={store}>
-      <Router>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Router>
-    </Provider>
-  ), document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
 
 serviceWorker.register();

de9e0f0792fa3997614c0bdbe2efe4096ae4eabb
ari7946 <mercado7946@gmail.com>
2022-07-01 17:58:08 -0700

    Set up theme and global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,19 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../features/_global/redux/store';
+import { ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
+import { GlobalStyle } from '../styles/global-style';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<Provider store={store}>
-			<Component {...pageProps} />
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  );
 }
 
 export default MyApp;
